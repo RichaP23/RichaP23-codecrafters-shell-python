0417957ee98842205bd66a2a8e410e8f466367d4
RichaP23 <96613127+RichaP23@users.noreply.github.com>
2025-03-01 02:47:15 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -24,7 +24,6 @@ def quotedText(text):
                 word+=i
         else:
             word+=i
-    print(textList)
     return textList
 def find_command(command):
     paths = os.environ.get('PATH') or ""    
