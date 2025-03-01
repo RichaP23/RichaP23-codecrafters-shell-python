0f3ce58ab50de1649ed18441317b7f85b428f41f
RichaP23 <96613127+RichaP23@users.noreply.github.com>
2025-03-01 22:24:19 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -95,38 +95,30 @@ def main():
                     if(flag):
                         flag=False
                         os.chdir(dir)
+                    
                 except FileNotFoundError:
                     print(f"{first_word}: {dir}: No such file or directory")
             case _:
-                try:
-                    found = False
-                    built_inPath=subprocess.run(["which",first_word],capture_output=True,text=True,check=True)
-                    #pathFound=os.system(f"which {first_word[1:-1]}")
-                    #print(built_inPath.returncode)
-                    for path in paths:
-                        #and os.access(executable_path, os.X_OK)
-                        executable_path = path+"/"+first_word
-                        pathExecutable=find_command(command.split(" ",1)[0])
-                        if pathExecutable:
-                            found = True
-                            try:
-                                #os.system(pathExecutable+" "+command.split(" ",1)[1])
-                                os.system(command)
-                            except FileNotFoundError:
-                                print(f"{first_word}: command not found")
-                            break
-                        #len(built_inPath.split(" "))==1
-                        elif built_inPath.returncode==0:
-                            try:
-                                os.system(command) 
-                            except FileNotFoundError:
-                                print(f"{first_word}: command not found")
-                            break
-                        else: 
-                            print(f"{command}: command not found")
-                            break
-                except subprocess.CalledProcessError:
-                    print(f"{command}: command not found")
+                found = False
+                built_inPath=subprocess.run(["which",first_word],capture_output=True,text=True)
+                for path in paths:
+                    #and os.access(executable_path, os.X_OK)
+                    executable_path = path+"/"+first_word
+                    pathExecutable=find_command(command.split(" ",1)[0])
+                    if pathExecutable:
+                        found = True
+                        try:
+                            #os.system(pathExecutable+" "+command.split(" ",1)[1])
+                            os.system(command)
+                        except FileNotFoundError:
+                            print(f"{first_word}: command not found")
+                        break
+                    elif(built_inPath!=f"{first_word} not found"):
+                        os.system(command)
+                        break
+                    else: 
+                        print(f"{command}: command not found")
+                        break
         
         
 
